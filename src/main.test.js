779833f17da5a8c7ca66f7555a49420c09f0a982
Denny Biasiolli <denny.biasiolli@gmail.com>
2020-08-05 22:55:08 +0200

    src: writing data to mongodb

--- src/main.test.js
+++ src/main.test.js
@@ -1,6 +1,9 @@
+const data = require('./data');
 const { main } = require('./main');
 const services = require('./services');
 
+jest.mock('./data');
+data.writeDataArrayWithoutException.mockImplementation(() => { });
 jest.mock('./services');
 services.getOperationTasks.mockImplementation(() => [
   { operationType: { _id: 1, discipline: 'abc123' } },
@@ -14,9 +17,28 @@ services.getOperationTasks.mockImplementation(() => [
   { operationType: { _id: 1, discipline: 'abc123' } },
   { operationType: { _id: 3, discipline: 'efg789' } },
 ]);
-services.getDiscipline.mockImplementation(() => ['abc123', 'cde456', 'efg789']);
+services.getDiscipline
+  .mockReturnValueOnce(Promise.resolve({ _id: 1, name: 'abc123' }))
+  .mockReturnValueOnce(Promise.resolve({ _id: 2, name: 'cde456' }))
+  .mockReturnValueOnce(Promise.resolve({ _id: 3, name: 'efg789' }))
+  ;
 
-test('main', () => {
-  main();
-  expect(services.getOperationTasks).toHaveBeenCalled();
+test('main', async () => {
+  await main();
+  expect(services.getOperationTasks).toHaveBeenCalledWith();
+  expect(services.getDiscipline).toHaveBeenCalledWith('abc123');
+  expect(services.getDiscipline).toHaveBeenCalledWith('cde456');
+  expect(services.getDiscipline).toHaveBeenCalledWith('efg789');
+  expect(data.writeDataArrayWithoutException).toHaveBeenCalledWith(
+    'operationTypes', [
+    { _id: 1, discipline: 'abc123' },
+    { _id: 2, discipline: 'cde456' },
+    { _id: 3, discipline: 'efg789' },
+  ]);
+  expect(data.writeDataArrayWithoutException).toHaveBeenCalledWith(
+    'disciplines', [
+    { _id: 1, name: 'abc123' },
+    { _id: 2, name: 'cde456' },
+    { _id: 3, name: 'efg789' },
+  ]);
 });
